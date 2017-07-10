c24896d5311d3ea6041208abaffd6238d35c4ec9
15291783517 <848671907@qq.com>
2017-07-10 19:45:53 +0800

    first commit

---
+++ app/src/main/java/com/example/fragmentbestpractice/News.java
@@ -0,0 +1,27 @@
+package com.example.fragmentbestpractice;
+
+/**
+ * Created by 王宇飞 on 2017/4/8/008.
+ */
+
+public class News {
+
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.java
@@ -0,0 +1,40 @@
+package com.example.fragmentbestpractice;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class NewsContentActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    public static void actionStart(Context context, String newsTitle, String newsContent) {
+        Intent intent = new Intent(context, NewsContentActivity.class);
+        intent.putExtra("new_title", newsTitle);
+        intent.putExtra("new_content", newsContent);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_content);
+        String newsTitle = getIntent().getStringExtra("news_title");
+        String newsContent = getIntent().getStringExtra("news_content");
+        NewsContentFragment newsContentFragment = (NewsContentFragment)
+        getSupportFragmentManager().findFragmentById(R.id.news_content_fragment);
+        newsContentFragment.refresh(newsTitle, newsContent);
+    }
+
+}
